f5a700d5d1b2e10e121bb1eaf1e7ab9f6538ac18
ElhSaf <elham_abdul@hotmail.com>
2022-05-11 13:06:38 +0200

    Båda G delarna klara

---
+++ WORKSPACE/Lektion 7/lektion7.js
@@ -0,0 +1,44 @@
+if (true){
+    var variabelVar = "this is variabel"   //var är global varibael //
+    let variabelLet = "this is Let" // let är bara i den kontexten eller i dessa måsvingar bara utanför finns inte variabeln
+
+    console.log(variabelVar);   
+    console.log(variabelLet);
+}
+
+console.log(variabelVar);   //den frsta kommer funka
+console.log(variabelLet); // denna kommer ej funka 
+
+
+//SWITCH satser
+let stoplight = "yellow"
+switch(stoplight) {
+case "red" :
+  
+    console.log("Stop") ;  
+  
+break;
+
+case "yellow" : 
+
+console.log("chillaa");
+
+break;
+
+case "green":
+  console.log("go")
+
+default:
+    
+    console.log("jallaaa");
+break;
+
+}
+
+// en typisk FUNCTION
+//tar emot data 
+// gör något med inout
+// retunerar output
+
+
+
